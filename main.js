e46916805719c0f969f2c236b5684a07e5aac792
solstefan <122591684+solstefan@users.noreply.github.com>
2023-06-06 22:14:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+const loader = new GLTFLoader();
+
+loader.load('Files/blocbot.glb', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
