c5b1a25b97f52a7d8a79636bf471a9030f4a4f52
solstefan <122591684+solstefan@users.noreply.github.com>
2023-06-07 00:19:32 -0400

    Update main.js
    new fix

--- main.js
+++ main.js
@@ -1,42 +1,103 @@
-import {
-    Camera,
-    Color,
-    Scene,
-    PerspectiveCamera,
-    WebGLRenderer,
-    AmbientLight,
-  } from "https://cdn.skypack.dev/three@0.132.2";
-  
-  import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/GLTFLoader.js';
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>BlocBots</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content type="model/gltf-binary" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+		<link type="text/css" rel="stylesheet" href="main.css">
+	</head>
 
-const container = document.querySelector('#scene-container');
-const scene = new Scene();
-const renderer = new WebGLRenderer();
-const hlight = new AmbientLight (0x404040,100);
-const fov = 80;
-const aspect = screen.width / screen.height;
-const near = 0.1;
-const far = 100; 
-const camera = new PerspectiveCamera(fov, aspect, near, far);
-const loader = new GLTFLoader();
+	<body>
+		
+			
+		</div>
 
-scene.background = new Color('white');
-camera.position.set(0, 50, 100);
-scene.add(hlight);
-renderer.setSize(screen.width, screen.height);
-renderer.setPixelRatio(window.devicePixelRatio);
-container.append(renderer.domElement);
+		<!-- Import maps polyfill -->
+		<!-- Remove this when import maps will be widely supported -->
+		<script async src="https://unpkg.com/es-module-shims@1.6.3/dist/es-module-shims.js"></script>
 
-loader.load('tree.glb', function(gltf){
-  const car = gltf.scene.children[0];
-  car.scale.set(1.5,1.5,1.5);
-  scene.add(gltf.scene);
-  animate();
-});
-renderer.render(scene, camera);
+		<script type="importmap">
+			{
+				"imports": {
+					"three": "../build/three.module.js",
+					"three/addons/": "./jsm/"
+				}
+			}
+		</script>
 
-function animate() {
-    renderer.render(scene,camera);
-    requestAnimationFrame(animate);
-  }
+		<script type="module">
 
+			import * as THREE from 'three';
+
+			import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+			import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+			let camera, scene, renderer;
+
+			init();
+			render();
+
+			function init() {
+
+				const container = document.createElement( 'div' );
+				document.body.appendChild( container );
+
+				camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
+				camera.position.set( - 1.8, 0.6, 2.7 );
+
+				scene = new THREE.Scene();
+
+						// model
+
+						const loader = new GLTFLoader().setPath( 'files//blocbot.glb' );
+						loader.load( 'https://storage.googleapis.com/blocbots/blocbot.glb', function ( gltf ) {
+
+							scene.add( gltf.scene );
+
+							render();
+
+						} );
+
+					} );
+
+				renderer = new THREE.WebGLRenderer( { antialias: true } );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				renderer.toneMapping = THREE.ACESFilmicToneMapping;
+				renderer.toneMappingExposure = 1;
+				container.appendChild( renderer.domElement );
+
+				const controls = new OrbitControls( camera, renderer.domElement );
+				controls.addEventListener( 'change', render ); // use if there is no animation loop
+				controls.minDistance = 2;
+				controls.maxDistance = 10;
+				controls.target.set( 0, 0, - 0.2 );
+				controls.update();
+
+				window.addEventListener( 'resize', onWindowResize );
+
+			}
+
+			function onWindowResize() {
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+				render();
+
+			}
+
+			//
+
+			function render() {
+
+				renderer.render( scene, camera );
+
+			}
+
+		</script>
+
+	</body>
+</html>
