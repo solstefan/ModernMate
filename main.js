b584fcfb0283197790a461eec5592bbc306f5afd
solstefan <122591684+solstefan@users.noreply.github.com>
2023-06-06 22:53:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
-import { GLTFLoader } from 'https://cdn.skypack.dev/three/examples/jsm/loaders/GLTFLoader';
+import {
+    Camera,
+    Color,
+    Scene,
+    PerspectiveCamera,
+    WebGLRenderer,
+    AmbientLight,
+  } from "https://cdn.skypack.dev/three@0.132.2";
+  
+  import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/GLTFLoader.js';
 
+const container = document.querySelector('#scene-container');
+const scene = new Scene();
+const renderer = new WebGLRenderer();
+const hlight = new AmbientLight (0x404040,100);
+const fov = 80;
+const aspect = screen.width / screen.height;
+const near = 0.1;
+const far = 100; 
+const camera = new PerspectiveCamera(fov, aspect, near, far);
 const loader = new GLTFLoader();
 
-loader.load('Files/scene.gltf', function ( gltf ) {
+scene.background = new Color('white');
+camera.position.set(0, 50, 100);
+scene.add(hlight);
+renderer.setSize(screen.width, screen.height);
+renderer.setPixelRatio(window.devicePixelRatio);
+container.append(renderer.domElement);
 
-	scene.add( gltf.scene );
+loader.load('tree.glb', function(gltf){
+  const car = gltf.scene.children[0];
+  car.scale.set(1.5,1.5,1.5);
+  scene.add(gltf.scene);
+  animate();
+});
+renderer.render(scene, camera);
 
-}, undefined, function ( error ) {
-
-	console.error( error );
-
-} );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+function animate() {
+    renderer.render(scene,camera);
+    requestAnimationFrame(animate);
+  }
 
